5642cde8b12b5929a62f3dff957ee0d67b483551
aantonchev@ivi.ru <aantonchev@ivi.ru>
2022-09-13 01:58:19 +0300

    Add bash script conf-replica

--- db/conf-replica.sh
+++ db/conf-replica.sh
@@ -19,7 +19,7 @@
 DB=laba_db
 DUMP_FILE="/tmp/$DB-export-$(date +"%Y%m%d%H%M%S").sql"
 
-USER=root
+USER=repl
 PASS="Qoj3R!4GIgeq@Myz@BZ@SYSoxx"
 
 MASTER_HOST=172.20.0.10
@@ -32,9 +32,12 @@ SLAVE_HOSTS=(172.20.0.20)
 ##
 
 echo "MASTER: $MASTER_HOST"
-
+echo "server-id = 1
+	  log-bin=mysql-bin.log
+	  binlog_do_db = $DB" >>  /etc/my.cnf
+systemctl restart mysqld.service
 mysql -h $MASTER_HOST "-u$USER" "-p$PASS" $DB <<-EOSQL &
-	GRANT REPLICATION SLAVE ON *.* TO '$USER'@'172.0.0.0' IDENTIFIED BY '$PASS';
+	GRANT REPLICATION SLAVE ON *.* TO '$USER'@'%' IDENTIFIED BY '$PASS';
 	FLUSH PRIVILEGES;
 	FLUSH TABLES WITH READ LOCK;
 	DO SLEEP(3600);
@@ -70,6 +73,11 @@ echo "  - Master database unlocked"
 for SLAVE_HOST in "${SLAVE_HOSTS[@]}"
 do
 	echo "SLAVE: $SLAVE_HOST"
+	echo "server-id = 2
+		  relay-log=mysql-relay-bin.log
+		  log_bin=mysql-bin.log
+		  binlog_do_db = laba_db"  >>  /etc/my.cnf
+	systemctl restart mysqld.service
 	echo "  - Creating database copy"
 	mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" -e "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB;"
 	scp $DUMP_FILE $SLAVE_HOST:$DUMP_FILE >/dev/null
@@ -80,7 +88,7 @@ do
 		STOP SLAVE;
 		CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',
 		MASTER_USER='$USER',
-		MASTER_PASSWORD='$USER',
+		MASTER_PASSWORD='$PASS',
 		MASTER_LOG_FILE='$LOG_FILE',
 		MASTER_LOG_POS=$LOG_POS;
 		START SLAVE;
--- db/db-init.sh
+++ db/db-init.sh
@@ -9,8 +9,9 @@ systemctl start mysqld.service
 mysqladmin --user root --password="$tempRootDBPass" password "$mysqlRootPass"
 mysql -u root --password="$mysqlRootPass" <<EOF
   CREATE DATABASE laba_db;
-  CREATE USER 'root'@'172.0.0.0' IDENTIFIED BY '$mysqlRootPass';
-  GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.0.0.0' WITH GRANT OPTION;
+  CREATE USER 'repl'@'%' IDENTIFIED BY '$mysqlRootPass';
+  ALTER USER 'repl'@'%' IDENTIFIED WITH mysql_native_password BY 'Qoj3R!4GIgeq@Myz@BZ@SYSoxx';
+  GRANT ALL PRIVILEGES ON *.* TO 'repl'@'%' WITH GRANT OPTION;
   FLUSH PRIVILEGES;
 EOF
 systemctl status mysqld.service
