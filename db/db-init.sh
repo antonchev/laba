e89627b54241a22549982f84a40650e11d57d529
aantonchev@ivi.ru <aantonchev@ivi.ru>
2022-09-12 19:54:56 +0300

    Add bash script init-db

--- db/db-init.sh
+++ db/db-init.sh
@@ -3,9 +3,9 @@ mysqlRootPass="$(pwmake 128)"
 tempRootDBPass="`grep 'temporary.*root@localhost' /var/log/mysqld.log | tail -n 1 | sed 's/.*root@localhost: //'`"
 echo "TEMP PASS $tempRootDBPass"
 echo ' -> Setting up new mysql server root password'
-systemctl stop mysqld
+systemctl stop mysqld.service
 rm -rf /var/lib/mysql/*logfile*
-systemctl start mysqld
+systemctl start mysqld.service
 echo "Auth"
 mysqladmin -u root --password="$tempRootDBPass" password "$mysqlRootPass"
 systemctl status mysqld.service
